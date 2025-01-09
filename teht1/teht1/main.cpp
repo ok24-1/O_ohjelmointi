9dd9f60098e8c7c0b0956b314a95ba2e134ea854
ok24-1 <okovanen24@students.oamk.fi>
2025-01-09 22:36:05 +0200

    Job done

---
+++ teht1/teht1/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int game(int);
+
+using namespace std;
+
+int main(){
+
+    int maxnum;
+
+    cout << "Syota pelin ylaraja lukuna." << endl;
+    cin >> maxnum;
+    cout << "Arvaa satunnainen luku valilta 1 - " << maxnum <<endl;
+    int score = game(maxnum);
+    cout <<"Arvauten maara:  "<< score + 1 << endl;
+}
+
+int game(int maxnum){
+
+    srand(time(0));
+    int randomNumber = rand() % maxnum + 1;
+
+    int userinput;
+    int userinputCounter;
+
+    while(1){
+        cin >> userinput;
+
+        if (userinput > randomNumber){
+            cout << "Lukusi on suurempi. Arvaa uusi luku:" << endl;
+            userinputCounter++;
+        }
+        if (userinput < randomNumber){
+            cout << "Lukusi on pienempi. Arvaa uusi luku:" << endl;
+            userinputCounter++;
+        }
+        if (userinput == randomNumber){
+            cout << "Lukusi on oikein. Arvaa uusi luku:" << endl;
+            break;
+        }
+    }
+    return userinputCounter;
+}
