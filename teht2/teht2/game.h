ff6be3b40424a272c1db6ecb711f35d3ccb4f567
ok24-1 <okovanen24@students.oamk.fi>
2025-01-15 23:41:03 +0200

    proto

---
+++ teht2/teht2/game.h
@@ -0,0 +1,18 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Game
+{
+public:
+    Game(int);
+    ~Game();
+    void play();
+private:
+    void printGameResult();
+    int maxNumber;
+    int playerGuess;
+    int randomNumber;
+    int numOfGuesses;
+};
+
+#endif // GAME_H
