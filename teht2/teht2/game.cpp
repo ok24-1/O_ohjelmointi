43b3ddbf354fb5227c894764005496500b9a6b0b
ok24-1 <okovanen24@students.oamk.fi>
2025-01-15 23:51:05 +0200

    proto

---
+++ teht2/teht2/game.cpp
@@ -0,0 +1,50 @@
+#include "game.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+Game::Game(int)
+{
+    cout << "PELIN RAKENTAJA: Kysytaan suurin satunnainen numero" << endl;
+    cout << "Syota suurin satunnainen numero:" << endl;
+    cin >> maxNumber;
+    Game::play();
+}
+
+Game::~Game()
+{
+    cout << "PELIN TUHOAJA: Peli tuhottu muistista" << endl;
+}
+
+void Game::play(){
+
+    srand(time(0));
+    int randomNumber = rand() % maxNumber + 1;
+
+    cout << "Arvaa satunnainen luku valilta 1 - " << maxNumber <<endl;
+
+    while(1){
+        cin >> playerGuess;
+
+        if (playerGuess > randomNumber){
+            cout << "Lukusi on suurempi. Arvaa uusi luku:" << endl;
+            numOfGuesses++;
+        }
+        if (playerGuess < randomNumber){
+            cout << "Lukusi on pienempi. Arvaa uusi luku:" << endl;
+            numOfGuesses++;
+        }
+        if (playerGuess == randomNumber){
+            cout << "Luku " << playerGuess << " oli oikein" << endl;
+            break;
+        }
+    }
+    Game::printGameResult();
+}
+
+void Game::printGameResult()
+{
+    cout << "Arvauksia: " << playerGuess << endl;
+}
---
+++ teht2/teht2/main.cpp
@@ -0,0 +1,8 @@
+#include <game.h>
+
+int main()
+{
+
+    Game(0);
+    return 0;
+}
